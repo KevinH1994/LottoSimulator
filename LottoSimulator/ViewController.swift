9619f2ee0adc6c7bc778e1fbb85935ae470704d2
KevinH1994 <kevinhering2@web.de>
2022-08-26 14:15:36 +0200

    Fertig

--- LottoSimulator/ViewController.swift
+++ LottoSimulator/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
-
+    
+    let lottoZahlen = [Int](1...49)
+    
+    var zufälligeLottoZahlen = [Int]()
+    
+    
+    @IBOutlet weak var eins: UILabel!
+    @IBOutlet weak var zwei: UILabel!
+    @IBOutlet weak var drei: UILabel!
+    @IBOutlet weak var vier: UILabel!
+    @IBOutlet weak var fünf: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func genarieren(_ sender: Any) {
+        genarierZahlen()
+    }
+    func genarierZahlen(){
+        zufälligeLottoZahlen = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lottoZahlen) as! [Int]
+        eins.text = String(zufälligeLottoZahlen[0])
+        zwei.text = String(zufälligeLottoZahlen[1])
+        drei.text = String(zufälligeLottoZahlen[2])
+        vier.text = String(zufälligeLottoZahlen[3])
+        fünf.text = String(zufälligeLottoZahlen[4])
+    }
 }
 
